9cb22459c94bc6609557c6cb2af8cfbd7455909e
Abhishek <abhishek@kisanhub.com>
2016-07-27 19:39:26 +0530

    a

--- jsgoodparts/app/scripts/main.js
+++ jsgoodparts/app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+
+
+console.log("It's me ready..lolz");
+console.log("Well");
+
+document.writeln('Hello worlds');
+
+Function.prototype.method = function (name, func)   {
+    this.prototype[name] = func;
+    return this;
+};
+
