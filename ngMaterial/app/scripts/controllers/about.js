c21c274734b38df35065c16954b2a7414e4fdc23
Abhishek <abhishek@kisanhub.com>
2016-08-10 16:50:51 +0530

    learning djangoRestFramework

---
+++ ngMaterial/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngMaterialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngMaterialApp
+ */
+angular.module('ngMaterialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ngMaterial/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngMaterialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngMaterialApp
+ */
+angular.module('ngMaterialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
