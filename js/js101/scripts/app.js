8efb2085e556733f15ba50644a3aa0df9dd40c64
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-08-15 02:19:08 +0530

    chap 3 complete

--- js/js101/scripts/app.js
+++ js/js101/scripts/app.js
@@ -1,1 +1,22 @@
-var myVariable; //undefined
+var sum = 4 + 5, // 9
+    difference = 4 - 5, // -1
+    multipication = 4 * 5, // 20
+    quotient = 4 / 5, // .8
+    foo = 4 - 5 * (6 - 8),
+    bar = foo + 1,
+    foobar = "Hello" + ", " + "World!",
+    strnum = 4 + 5 + "7"; // Hello, World!
+
+/*
+ * multiline comment
+ */
+
+alert(sum);
+alert(difference);
+alert(foo);
+
+
+var foo1 = parseInt("34s57", 10), //34
+    foo2 = parseInt("s"); // NaN
+alert(foo1);
+alert(foo2);
