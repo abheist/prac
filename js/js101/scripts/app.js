7bbf237bbb14c20c689c6770f4c46f96dcb4cf2a
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-08-15 02:39:16 +0530

    chap 4 complete

--- js/js101/scripts/app.js
+++ js/js101/scripts/app.js
@@ -1,18 +1,22 @@
-var doSomething = function(paramOne, paramTwo)  {
+var doSomething = function(paramOne, paramTwo, fn)  {
     paramOne = paramOne + 3;
     paramOne = paramOne + 1;
     paramOne = paramOne * 8;
 
-    return paramOne * paramTwo;
+    return fn(paramOne, paramTwo);
+}
+
+function sum (paramOne, paramTwo) {
+    return paramOne + paramTwo;
 }
 
 // set one
-var foo = doSomething(2, 2);
+var foo = doSomething(2, 2, sum);
 
-// set two
-var bar = doSomething(3, 2);
-
-// function
+// set two & use of anonymous function
+var bar = doSomething(3, 2, function (paramOne, paramTwo) {
+    return paramOne * paramTwo;
+});
 
 alert(foo);
 alert(bar);
